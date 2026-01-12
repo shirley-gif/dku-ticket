7692b24ea1ff6312354f360388a8e97907a05aa5
shirley-gif <shirley.xyc@gmail.com>
2026-01-12 18:43:23 +0800

    Add pull-all/push-all scripts for clasp projects

---
+++ tools/pull-all.ps1
@@ -0,0 +1,12 @@
+﻿$projects = @(
+  "projects\dku_ticket_web_chat",
+  "projects\lib_ticket_api",
+  "projects\dkul_systems_ticket",
+  "projects\dku_ticket_core"
+)
+foreach ($p in $projects) {
+  Write-Host "== PULL $p =="
+  Push-Location $p
+  clasp pull
+  Pop-Location
+}
---
+++ tools/push-all.ps1
@@ -0,0 +1,12 @@
+﻿$projects = @(
+  "projects\dku_ticket_web_chat",
+  "projects\lib_ticket_api",
+  "projects\dkul_systems_ticket",
+  "projects\dku_ticket_core"
+)
+foreach ($p in $projects) {
+  Write-Host "== PUSH $p =="
+  Push-Location $p
+  clasp push -f
+  Pop-Location
+}
